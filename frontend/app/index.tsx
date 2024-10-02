9270138fee4d71ea4306142eb42939a0541f0b93
Igor Leon <igorleonsimim@gmail.com>
2024-10-02 16:19:27 -0300

    :rocket: Big change to the screen files on the front

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,93 +1,53 @@
-import { Link } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet, Image, TextInput, Button, ScrollView, TouchableHighlight } from 'react-native';
-// import myCustomStyles from './_styles';      // Aqui podem importar estilos 
+import { View, Text, StyleSheet, Image, ScrollView, Dimensions } from 'react-native';
+import Header from '../components/Header';
+import { Link } from 'expo-router';
 
 export default function HomeScreen() {
-
-  const [text, funcao] = React.useState('Useless Text');
-  const [numeroDeCliques, setNumeroDeCliques] = React.useState(0);
-
-  const submitValues = () => {
-    console.log(text);
-  }
-
-  const handleButtonClick = () => {
-    setNumeroDeCliques(numeroDeCliques + 1);
-  }
-
-  const handleTouch = () => {
-    console.log("Clicou na Imagem!!!!");
-  }
+  const { width } = Dimensions.get('window');
 
   return (
     <View style={styles.container}>
-      <ScrollView style={{maxWidth: 500}}>
-      <Text style={styles.title}>React Native é Show!</Text>
-      <View style={styles.meuBotao}>
-        <Button title={"Este botão foi pressionado " + numeroDeCliques + ' vezes!'} onPress={handleButtonClick}></Button>
-      </View>
-      <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-        <Text>Home</Text>
-        <View style={{backgroundColor: 'blue', padding: 50}}>
-          <Text style={{color: 'white'}}>Texto de uma view</Text>
-        </View>
-        
-        <View style={{backgroundColor: 'red', padding: 50}}>
-          <TouchableHighlight onPress={handleTouch}>
-            <Image style={styles.logo} source={{uri: 'https://miro.medium.com/v2/resize:fit:1024/1*xDi2csEAWxu95IEkaNdFUQ.png'}}></Image>
-          </TouchableHighlight>
-        </View>
-        <Link href="/cadastro">Ir para a tela de cadastro</Link>
-        <Link href="/listagem">Ir para a tela de listagem</Link>
-        <Link href="/login">Ir para a tela de login</Link>
-        <Link href="/sobre">Ir para a tela de sobre</Link>
-        <View>
-          <TextInput
-            style={styles.input}
-            onChangeText={funcao}
-            value={text}
+      <Header />
+
+      <ScrollView style={{ maxWidth: 500 }}>
+        <Text style={styles.title}>Bem-vindo à Gráfica Pelotense!</Text>
+
+        <View style={styles.imageContainer}>
+          <Image
+            style={[styles.image, { width: width * 0.4, height: (width * 0.4) * 0.6 }]}
+            source={{ uri: '../assets/images/frase-home.png' }}
+            resizeMode="contain"
           />
-          <Button title='Enviar dados' onPress={submitValues}></Button>
         </View>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        />
+
+        <Text style={styles.description}>
+          A Gráfica Pelotense oferece uma ampla gama de serviços de impressão, desde cartões de visita até livros de alta qualidade.
+          Trabalhamos com dedicação para entregar o melhor produto aos nossos clientes.
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <Link href="/contato" style={styles.button}>
+            <Text style={styles.buttonText}>Conheça nossos serviços</Text>
+          </Link>
+
+          <Link href="/contato" style={styles.button}>
+            <Text style={styles.buttonText}>Entre em contato</Text>
+          </Link>
+        </View>
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  meuBotao: {
-    margin: 20
-  },
   container: {
     flex: 1,
-    flexDirection:'column',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  logo: {
-    width: 250,
-    height: 250,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    backgroundColor: '#1E3A61',
+    padding: 20,
   },
   title: {
     marginTop: 16,
@@ -98,7 +58,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#61dafb',
     color: '#20232a',
     textAlign: 'center',
-    fontSize: 30,
+    fontSize: 28,
     fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  imageContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  image: {
+    borderRadius: 20,
+  },
+  description: {
+    fontSize: 16,
+    color: '#F5F5DC',
+    marginBottom: 20,
+    textAlign: 'center',
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    maxWidth: 500,
+  },
+  button: {
+    backgroundColor: '#61dafb',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    textAlign: 'center',
+    marginHorizontal: 10,
+    flex: 1,
+  },
+  buttonText: {
+    color: '#20232a',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
